819ad35ca0251f07cd7b267ff515b98f05b843f0
sahilchalke0001 <sahilchalke0001@gmail.com>
2025-03-20 16:09:54 +0530

    pyaudio

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-#!/usr/bin/env bash
-# Install PortAudio dependencies
+#!/bin/bash
+
+# Install system dependencies required for PyAudio
 apt-get update && apt-get install -y portaudio19-dev
+
+# Install project dependencies
+pip install -r requirements.txt
