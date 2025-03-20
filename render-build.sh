1ee7bdd1d7b965851ca4960a39199aae1338f284
sahilchalke0001 <sahilchalke0001@gmail.com>
2025-03-20 17:30:44 +0530

    pyaudio

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Update package list and install portaudio dependencies
-apt-get update && apt-get install -y portaudio19-dev python3-dev python3-pip gcc
-
-# Install required packages
-pip install -r requirements.txt
-
