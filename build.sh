4779b746682130aa7c82a13acd2138157071717e
sahilchalke0001 <sahilchalke0001@gmail.com>
2025-03-20 17:43:27 +0530

    Add build script for Render

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-# Install system-level dependencies
-apt-get update && apt-get install -y libasound2-dev portaudio19-dev libportaudio2 libportaudiocpp0
-
-
-# Install required Python packages
-pip install -r requirements.txt
+# Update package lists
+apt-get update
+# Install required dependencies for PyAudio and PortAudio
+apt-get install -y libasound2-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg
