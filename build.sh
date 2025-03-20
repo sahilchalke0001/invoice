285bf4353aff3eb72883b3a8a4dba307efcd2ed3
sahilchalke0001 <sahilchalke0001@gmail.com>
2025-03-20 16:27:09 +0530

    Add build script to install portaudio again s

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install system dependencies required for PyAudio
-apt-get update && apt-get install -y portaudio19-dev
-
-# Install project dependencies
-pip install -r requirements.txt
